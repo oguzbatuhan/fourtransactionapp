435a8d6f4dc2f5991810a24b11f439d02b382923
Oğuz Batuhan Çözeli <oguzbatuhanczl@gmail.com>
2024-04-17 20:32:08 +0300

    fourtransactionapp

--- fourtransactionapp/ViewController.swift
+++ fourtransactionapp/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UITextField!
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func collection(_ sender: Any) {
+        if let sayı = Double(text.text!){
+            if let sayı1 = Double(text1.text!){
+                let sonuc = sayı+sayı1
+                text2.text = String(sonuc)
+            }
+        }
+    }
+    @IBAction func extraction(_ sender: Any) {
+        if let sayı = Double(text.text!){
+            if let sayı1 = Double(text1.text!){
+                let sonuc = sayı-sayı1
+                text2.text = String(sonuc)
+            }
+        }
+    }
+    @IBAction func impact(_ sender: Any) {
+        if let sayı = Double(text.text!){
+            if let sayı1 = Double(text1.text!){
+                let sonuc = sayı*sayı1
+                text2.text = String(sonuc)
+            }
+        }
+    }
+    @IBAction func divide(_ sender: Any) {
+        if let sayı = Double(text.text!){
+            if let sayı1 = Double(text1.text!){
+                let sonuc = sayı/sayı1
+                text2.text = String(sonuc)
+            }
+        }
+    }
+    
 
-
+    
+    
+    
+    
 }
 
